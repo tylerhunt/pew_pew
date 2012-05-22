3609d5d744d6862d7e317652f041449618cb85e9
Tyler Hunt <tyler@tylerhunt.com>
2012-05-22 11:33:55 -0700

    Add specs for resource options.

--- spec/pew_pew/resources/stats_spec.rb
+++ spec/pew_pew/resources/stats_spec.rb
@@ -1,7 +1,22 @@
 require 'spec_helper'
 
 describe PewPew::Resources::Stats, :resource do
-  let(:resource) { described_class.new(client) }
+  let(:options) { {} }
+  let(:resource) { described_class.new(client, options) }
+
+  context 'options' do
+    subject { resource }
+
+    context 'without options' do
+      its(:domain) { should == client.config.domain }
+    end
+
+    context 'with option :domain' do
+      let(:options) { { domain: 'pewpew.test' } }
+
+      its(:domain) { should == 'pewpew.test'}
+    end
+  end
 
   context '#all' do
     subject { resource.all }
