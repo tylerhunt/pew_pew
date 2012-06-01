7b2b5ea4f009e6eca95ed02c48d00c69f7eb101d
Tyler Hunt <tyler@tylerhunt.com>
2012-06-01 13:04:27 -0700

    Add specs for Stats#all with actual results.

--- spec/pew_pew/resources/stats_spec.rb
+++ spec/pew_pew/resources/stats_spec.rb
@@ -26,7 +26,16 @@ describe PewPew::Resources::Stats, :resource do
     specify { should be_success }
 
     its(:status) { should == 200 }
-    its(:total_count) { should == 0 }
-    its(:items) { should == [] }
+    its(:total_count) { should == 10 }
+
+    context 'item' do
+      subject { response.items.first }
+
+      its(:created_at) { should == 'Fri, 01 Jun 2012 00:00:00 GMT' }
+      its(:event) { should == 'sent' }
+      its(:id) { should == '4fc837558393c1618263d76c' }
+      its(:tags) { should == {} }
+      its(:total_count) { should == 11 }
+    end
   end
 end
