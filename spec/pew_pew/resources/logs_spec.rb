10376458af0dacd64ab5a150f69d694d5a84b442
Tyler Hunt <tyler@tylerhunt.com>
2012-06-01 13:07:45 -0700

    Add specs for Logs#all with actual results.

--- spec/pew_pew/resources/logs_spec.rb
+++ spec/pew_pew/resources/logs_spec.rb
@@ -1,3 +1,5 @@
+# encoding: UTF-8
+
 require 'spec_helper'
 
 describe PewPew::Resources::Logs, :resource do
@@ -26,7 +28,16 @@ describe PewPew::Resources::Logs, :resource do
     specify { should be_success }
 
     its(:status) { should == 200 }
-    its(:total_count) { should == 0 }
-    its(:items) { should == [] }
+    its(:total_count) { should == 27 }
+
+    context 'item' do
+      subject { response.items.first }
+
+      its(:created_at) { should == 'Fri, 01 Jun 2012 04:19:04 GMT' }
+      its(:hap) { should == 'delivered' }
+      its(:message) { should == "Delivered:  postmaster@pewpew.mailgun.org → pewpew@devoh.com 'Test'" }
+      its(:message_id) { should == '4fc84273c474_48dd3fe70cc34cd05949d@rouge.local.mail' }
+      its(:type) { should == 'info' }
+    end
   end
 end
